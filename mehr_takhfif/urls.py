8ffda98e7b62fd813f21f9753ea9142e7e481c85
soheil <soheilravasani@gmail.com>
2020-01-26 20:29:19 +0330

    update settings
    Former-commit-id: 20f92baa8b7abc57903fd4df9680675825a53e5e
    Former-commit-id: 446a1d1ef421742acdcc5a00ca0fe27b70c7e6be

--- mehr_takhfif/urls.py
+++ mehr_takhfif/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('server.urls')),
-    path('box_admin/', include('admin.urls')),
+    path('admin/', include('admin.urls')),
 ]
 
 if settings.DEBUG:
