3dca7e2cfe418c4fa1267712d3c295a6bc193167
soheil <soheilravasani@gmail.com>
2020-01-30 20:24:55 +0330

    add admin login
    Former-commit-id: 85ba7c94eb6e5a64ab9b3d08a8eac679867a6394
    Former-commit-id: 62db80b7946dab2cce31ccf8d52c492436d7b072

--- admin/decorator.py
+++ admin/decorator.py
@@ -4,9 +4,10 @@ import re
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.http import JsonResponse
-from django.core.exceptions import FieldDoesNotExist
+from django.core.exceptions import FieldDoesNotExist, ValidationError
 from django.db.utils import IntegrityError
 from server.views.utils import res_code
+from server.error import *
 
 
 def error_handler(func):
@@ -16,13 +17,16 @@ def error_handler(func):
             return func(*args, **kwargs)
         except AssertionError:
             traceback.print_exc()
-            return JsonResponse({}, status=res_code['forbidden'])
+            return JsonResponse({}, status=res_code['token_issue'])
         except IntegrityError as e:
             traceback.print_exc()
             pattern = r'(\((\w+)\))='
             field = re.search(pattern, str(e))[2]
             return JsonResponse({'type': 'duplicate', 'field': field}, status=res_code['integrity'])
-        except FieldDoesNotExist:
+        except (FieldDoesNotExist, ValidationError):
             traceback.print_exc()
             return JsonResponse({'message': 'fields name is incorrect'}, status=res_code['bad_request'])
+        except AuthError:
+            traceback.print_exc()
+            return JsonResponse({'message': 'authentication failed'}, status=res_code['forbidden'])
     return wrapper
