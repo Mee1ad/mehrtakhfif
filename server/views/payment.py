f0886921032e694695a910a226238beacc9d282b
soheil <soheilravasani@gmail.com>
2020-06-08 13:39:28 +0430

    fix task interval

--- server/views/payment.py
+++ server/views/payment.py
@@ -147,7 +147,7 @@ class PaymentRequest(View):
             task_name = f'{invoice.id}: cancel reservation'
             # args = []
             kwargs = {"invoice_id": invoice.id, "task_name": task_name}
-            invoice.sync_task = add_one_off_job(name=task_name, kwargs=kwargs, interval=1,
+            invoice.sync_task = add_one_off_job(name=task_name, kwargs=kwargs, interval=15,
                                                 task='server.tasks.cancel_reservation')
             # basket.active = False
             basket.sync = 1  # reserved
