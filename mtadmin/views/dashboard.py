37bc1357b989f76c7129f45d5a8cacb0e560e8c9
soheil <soheilravasani@gmail.com>
2020-06-10 18:36:53 +0430

    test

--- mtadmin/views/dashboard.py
+++ mtadmin/views/dashboard.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from mtadmin.dashboard_serializer import *
 from datetime import datetime, timedelta
 from jdatetime import datetime as jdatetime, timedelta as jtimedelta
+from server.models import *
 
 
 class DateProductCount(AdminView):
@@ -25,14 +26,74 @@ class DateProductCount(AdminView):
 
                 cp.append(created_products)
                 up.append(updated_products)
-            boxes_list.append({'id': box.id, 'name': box.name, 'created_products': cp,
-                               'updated_products': up, 'settings': box.settings})
+            boxes_list.append({'id': box.id, 'name': box.name['fa'], 'created_products': cp,
+                               'updated_products': up, 'setting': box.settings})
         data = {'label': labels, 'boxes': boxes_list}
 
         return JsonResponse(data)
 
 
 class ProductCount(AdminView):
+    @pysnooper.snoop()
     def get(self, request):
-        boxes = Box.objects.all()
-        return JsonResponse({'boxes': ProductCountSchema().dump(boxes, many=True)})
+        products = Product.objects.all()
+        for product in products:
+            data = product.properties
+            if data:
+                res = {"data": {
+                    "ar": {
+                        "items": []
+                    },
+                    "en": {
+                        "items": []
+                    },
+                    "fa": {
+                        "items": []
+                    }
+                }}
+
+                property = {
+                    "id": 1,
+                    "type": {
+                        "key": "attributes",
+                        "label": "ویژگی ها"
+                    },
+                    "items": []
+                }
+                usage_condition = {
+                    "id": 2,
+                    "type": {
+                        "key": "term_of_use",
+                        "label": "شرایط استفاده"
+                    },
+                    "items": []
+                }
+
+                if 'fa' in data:
+                    if 'property' in data["fa"]:
+                        for item in data["fa"]["property"]:
+                            property["items"].append({
+                                "id": item["id"],
+                                "icon": item["icon"],
+                                "text": item["text"],
+                                "priority": item["priority"]
+                            })
+                        res["data"]["fa"]["items"].append(property)
+                if 'fa' in data:
+                    if 'usage_condition' in data['fa']:
+                        if data["fa"]["usage_condition"]:
+                            for item in data["fa"]["usage_condition"]:
+                                usage_condition["items"].append({
+                                    "id": item["id"],
+                                    "icon": item["icon"],
+                                    "text": item["text"],
+                                    "priority": item["priority"]
+                                })
+                            res["data"]["fa"]["items"].append(usage_condition)
+
+                product.properties = res
+                product.save()
+        print('tammam')
+        return JsonResponse({'message': 'taamaam'})
+        # boxes = Box.objects.all()
+        # return JsonResponse({'boxes': ProductCountSchema().dump(boxes, many=True)})
