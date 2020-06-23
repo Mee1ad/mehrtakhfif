2c4b24f7f44a350b0ffbae3c187c3c6690c9613e
soheil <soheilravasani@gmail.com>
2020-06-23 12:48:04 +0430

    fix basket

--- server/views/client/box.py
+++ server/views/client/box.py
@@ -84,7 +84,7 @@ class Filter(View):
         if params['related']:
             query = Q(verify=True, **params['filter']) | Q(verify=True, **params['related'])
         products = Product.objects.annotate(**params['annotate']).filter(query, Q(**disable), ~Q(type=5)).order_by(
-            params['order'], '-id').distinct('id', params['order'].replace('-', ''))
+            params['order'], 'id').distinct('id', params['order'].replace('-', ''))
         # params['order']).order_by('-id').distinct('id')
         pg = get_pagination(request, products, MinProductSchema)
         return JsonResponse(pg)
