f9b3d790452daa33e22e8d87952b2c53ead74ecb
soheil <soheilravasani@gmail.com>
2020-07-02 18:22:16 +0430

    fix search

--- mtadmin/views/views.py
+++ mtadmin/views/views.py
@@ -139,8 +139,8 @@ class Search(AdminView):
         products_id = []
         s = ProductDocument.search()
         type_query = Q('bool', should=[Q("match", type=product_type) for product_type in product_types])
-        r = s.query('match', box_id=box_id[0]).query(type_query).query('match', name_fa=q[0])
-        if r.count() == 0 and not q[0]:
+        r = s.query('match', box_id=box_id[0]).query(type_query).query('match', name_fa=q)
+        if r.count() == 0 and not q:
             r = s.query('match', box_id=box_id[0]).query(type_query).query('match_all')[:10]
         [products_id.append(product.id) for product in r]
         products = Product.objects.in_bulk(products_id)
@@ -150,7 +150,7 @@ class Search(AdminView):
     def supplier(self, q, **kwargs):
         items = []
         s = SupplierDocument.search()
-        r = s.query("multi_match", query=q[0],
+        r = s.query("multi_match", query=q,
                     fields=['first_name', 'last_name', 'username']).filter("term", is_supplier="true")[:5]
         if r.count() == 0:
             r = s.query("match_all").filter("term", is_supplier="true")[:5]
