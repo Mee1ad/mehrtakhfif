a416cc74e68e29d7c51946d9983f09b4831be7ad
Meelad <soheilravasani@gmail.com>
2020-12-26 17:01:10 +0330

    bug fix

--- server/utils.py
+++ server/utils.py
@@ -219,8 +219,9 @@ def get_invoice_file(request, invoice=None, invoice_id=None, user={}):
         invoice_dict['shipping_invoice']['tax'] = get_tax(2, invoice_dict['shipping_invoice']['amount'], 0)
     invoice_dict['user'] = UserSchema().dump(invoice.user)
     try:
-        invoice_dict['date'] = jdatetime.datetime.fromgregorian(datetime=add_minutes(invoice.payed_at)) \
-            .strftime("%Y/%m/%d")
+        # invoice_dict['date'] = jdatetime.datetime.fromgregorian(datetime=add_minutes(invoice.payed_at)) \
+        #     .strftime("%Y/%m/%d")
+        invoice_dict['date'] = to_jalali(invoice.payed_at)
     except ValueError:
         invoice_dict['date'] = '1399/99/99'
     invoice_dict['barcode'] = get_barcode(invoice.id)
