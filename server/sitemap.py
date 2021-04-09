644e5d691b2403c3520a57d155ceed483e3b880f
Meelad <soheilravasani@gmail.com>
2021-04-09 19:01:44 +0430

    add sitemap

---
+++ server/sitemap.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+
+from django.contrib.sitemaps import Sitemap
+
+from server.models import Product, Category, Tag
+
+
+class MySitemap(object):
+    def __init__(self, model):
+        self.model = model
+
+    def get_absolute_url(self):
+        return f"/{self.model}-sitemap.xml"
+
+
+class BaseSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 1
+
+    def items(self):
+        return [MySitemap("product"), MySitemap("tag"), MySitemap("category")]
+
+    def lastmod(self, obj):
+        return datetime.now()
+
+
+class ProductSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 1
+
+    def items(self):
+        return Product.objects.all().order_by('-id')
+
+    def lastmod(self, obj):
+        return obj.updated_at
+
+
+class CategorySitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.9
+
+    def items(self):
+        return Category.objects.all().order_by('-id')
+
+    def lastmod(self, obj):
+        return obj.updated_at
+
+
+class TagSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.8
+
+    def items(self):
+        return Tag.objects.all().order_by('-id')
+
+    def lastmod(self, obj):
+        return obj.updated_at
