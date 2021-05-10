949aac36bd2824bb88d3c8cc5d3bf0651ed2a525
Meelad <soheilravasani@gmail.com>
2021-05-10 12:06:04 +0430

    update django admin

--- server/admin.py
+++ server/admin.py
@@ -2,15 +2,106 @@ import pprint
 from datetime import date
 
 from django.contrib import admin
+from django.contrib import messages
+from django.contrib.admin import helpers
+from django.contrib.admin.exceptions import DisallowedModelAdminToField
+from django.contrib.admin.options import (
+    IS_POPUP_VAR, TO_FIELD_VAR, )
+from django.contrib.admin.utils import (
+    model_ngettext, )
+from django.contrib.admin.views.main import ChangeList
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Permission
 from django.contrib.postgres.fields.jsonb import KeyTextTransform
+from django.core.exceptions import (
+    PermissionDenied, )
+from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
+from django.template.response import SimpleTemplateResponse, TemplateResponse
 from django.utils.html import escape
+from django.utils.translation import gettext
 from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ngettext
 from prettyjson import PrettyJSONWidget
 from safedelete.admin import SafeDeleteAdmin
 
 from .models import *
+
+# Changelist settings
+ALL_VAR = 'all'
+ORDER_VAR = 'o'
+ORDER_TYPE_VAR = 'ot'
+PAGE_VAR = 'p'
+SEARCH_VAR = 'q'
+ERROR_FLAG = 'e'
+
+IGNORED_PARAMS = (
+    ALL_VAR, ORDER_VAR, ORDER_TYPE_VAR, SEARCH_VAR, IS_POPUP_VAR, TO_FIELD_VAR)
+
+
+class NoCountPaginator(Paginator):
+    @property
+    def count(self):
+        return 100  # Some arbitrarily large number,
+        # so we can still get our page tab.
+
+
+class MyChangeList(ChangeList):
+    def __init__(self, request, model, list_display, list_display_links,
+                 list_filter, date_hierarchy, search_fields, list_select_related,
+                 list_per_page, list_max_show_all, list_editable, model_admin, sortable_by):
+        self.model = model
+        self.opts = model._meta
+        self.lookup_opts = self.opts
+        if re.search(r"/\w+/\w+/\w+/\d+", request.path):
+            self.root_queryset = model_admin.model.objects.none()
+        else:
+            self.root_queryset = model_admin.get_queryset(request)
+        self.list_display = list_display
+        self.list_display_links = list_display_links
+        self.list_filter = list_filter
+        self.has_filters = None
+        self.date_hierarchy = date_hierarchy
+        self.search_fields = search_fields
+        self.list_select_related = list_select_related
+        self.list_per_page = list_per_page
+        self.list_max_show_all = list_max_show_all
+        self.model_admin = model_admin
+        self.preserved_filters = model_admin.get_preserved_filters(request)
+        self.sortable_by = sortable_by
+
+        # Get search parameters from the query string.
+        try:
+            self.page_num = int(request.GET.get(PAGE_VAR, 0))
+        except ValueError:
+            self.page_num = 0
+        self.show_all = ALL_VAR in request.GET
+        self.is_popup = IS_POPUP_VAR in request.GET
+        to_field = request.GET.get(TO_FIELD_VAR)
+        if to_field and not model_admin.to_field_allowed(request, to_field):
+            raise DisallowedModelAdminToField("The field %s cannot be referenced." % to_field)
+        self.to_field = to_field
+        self.params = dict(request.GET.items())
+        if PAGE_VAR in self.params:
+            del self.params[PAGE_VAR]
+        if ERROR_FLAG in self.params:
+            del self.params[ERROR_FLAG]
+
+        if self.is_popup:
+            self.list_editable = ()
+        else:
+            self.list_editable = list_editable
+        self.query = request.GET.get(SEARCH_VAR, '')
+        self.queryset = self.get_queryset(request)
+        self.get_results(request)
+        if self.is_popup:
+            title = gettext('Select %s')
+        elif self.model_admin.has_change_permission(request):
+            title = gettext('Select %s to change')
+        else:
+            title = gettext('Select %s to view')
+        self.title = title % self.opts.verbose_name
+        self.pk_attname = self.lookup_opts.pk.attname
 
 
 class SessionAdmin(admin.ModelAdmin):
@@ -41,9 +132,167 @@ from django.urls import reverse
 from django.utils.http import urlencode
 from django.utils.safestring import mark_safe
 from django.utils.translation import gettext as _
+from django.contrib.admin.options import csrf_protect_m, IncorrectLookupParameters
 
 
 class Base(SafeDeleteAdmin):
+    paginator = NoCountPaginator
+
+    @csrf_protect_m
+    def changelist_view(self, request, extra_context=None):
+        """
+        The 'change list' admin view for this model.
+        """
+        from django.contrib.admin.views.main import ERROR_FLAG
+        opts = self.model._meta
+        app_label = opts.app_label
+        if not self.has_view_or_change_permission(request):
+            raise PermissionDenied
+
+        try:
+            cl = self.get_changelist_instance(request)
+        except IncorrectLookupParameters:
+            # Wacky lookup parameters were given, so redirect to the main
+            # changelist page, without parameters, and pass an 'invalid=1'
+            # parameter via the query string. If wacky parameters were given
+            # and the 'invalid=1' parameter was already in the query string,
+            # something is screwed up with the database, so display an error
+            # page.
+            if ERROR_FLAG in request.GET:
+                return SimpleTemplateResponse('admin/invalid_setup.html', {
+                    'title': _('Database error'),
+                })
+            return HttpResponseRedirect(request.path + '?' + ERROR_FLAG + '=1')
+
+        # If the request was POSTed, this might be a bulk action or a bulk
+        # edit. Try to look up an action or confirmation first, but if this
+        # isn't an action the POST will fall through to the bulk edit check,
+        # below.
+        action_failed = False
+        selected = request.POST.getlist(helpers.ACTION_CHECKBOX_NAME)
+
+        actions = self.get_actions(request)
+        # Actions with no confirmation
+        if (actions and request.method == 'POST' and
+                'index' in request.POST and '_save' not in request.POST):
+            if selected:
+                response = self.response_action(request, queryset=cl.get_queryset(request))
+                if response:
+                    return response
+                else:
+                    action_failed = True
+            else:
+                msg = _("Items must be selected in order to perform "
+                        "actions on them. No items have been changed.")
+                self.message_user(request, msg, messages.WARNING)
+                action_failed = True
+
+        # Actions with confirmation
+        if (actions and request.method == 'POST' and
+                helpers.ACTION_CHECKBOX_NAME in request.POST and
+                'index' not in request.POST and '_save' not in request.POST):
+            if selected:
+                response = self.response_action(request, queryset=cl.get_queryset(request))
+                if response:
+                    return response
+                else:
+                    action_failed = True
+
+        if action_failed:
+            # Redirect back to the changelist page to avoid resubmitting the
+            # form if the user refreshes the browser or uses the "No, take
+            # me back" button on the action confirmation page.
+            return HttpResponseRedirect(request.get_full_path())
+
+        # If we're allowing changelist editing, we need to construct a formset
+        # for the changelist given all the fields to be edited. Then we'll
+        # use the formset to validate/process POSTed data.
+        formset = cl.formset = None
+
+        # Handle POSTed bulk-edit data.
+        if request.method == 'POST' and cl.list_editable and '_save' in request.POST:
+            if not self.has_change_permission(request):
+                raise PermissionDenied
+            FormSet = self.get_changelist_formset(request)
+            modified_objects = self._get_list_editable_queryset(request, FormSet.get_default_prefix())
+            formset = cl.formset = FormSet(request.POST, request.FILES, queryset=modified_objects)
+            if formset.is_valid():
+                changecount = 0
+                for form in formset.forms:
+                    if form.has_changed():
+                        obj = self.save_form(request, form, change=True)
+                        self.save_model(request, obj, form, change=True)
+                        self.save_related(request, form, formsets=[], change=True)
+                        change_msg = self.construct_change_message(request, form, None)
+                        self.log_change(request, obj, change_msg)
+                        changecount += 1
+
+                if changecount:
+                    msg = ngettext(
+                        "%(count)s %(name)s was changed successfully.",
+                        "%(count)s %(name)s were changed successfully.",
+                        changecount
+                    ) % {
+                              'count': changecount,
+                              'name': model_ngettext(opts, changecount),
+                          }
+                    self.message_user(request, msg, messages.SUCCESS)
+
+                return HttpResponseRedirect(request.get_full_path())
+
+        # Handle GET -- construct a formset for display.
+        elif cl.list_editable and self.has_change_permission(request):
+            FormSet = self.get_changelist_formset(request)
+            formset = cl.formset = FormSet(queryset=cl.result_list)
+
+        # Build the list of media to be used by the formset.
+        if formset:
+            media = self.media + formset.media
+        else:
+            media = self.media
+
+        # Build the action form and populate it with available actions.
+        if actions:
+            action_form = self.action_form(auto_id=None)
+            action_form.fields['action'].choices = self.get_action_choices(request)
+            media += action_form.media
+        else:
+            action_form = None
+
+        selection_note_all = ngettext(
+            '%(total_count)s selected',
+            'All %(total_count)s selected',
+            # cl.result_count
+            cl.result_count
+        )
+
+        context = {
+            **self.admin_site.each_context(request),
+            'module_name': str(opts.verbose_name_plural),
+            'selection_note': _('0 of %(cnt)s selected') % {'cnt': len(cl.result_list)},
+            'selection_note_all': selection_note_all % {'total_count': cl.result_count},
+            'title': cl.title,
+            'is_popup': cl.is_popup,
+            'to_field': cl.to_field,
+            'cl': cl,
+            'media': media,
+            'has_add_permission': self.has_add_permission(request),
+            'opts': cl.opts,
+            'action_form': action_form,
+            'actions_on_top': self.actions_on_top,
+            'actions_on_bottom': self.actions_on_bottom,
+            'actions_selection_counter': self.actions_selection_counter,
+            'preserved_filters': self.get_preserved_filters(request),
+            **(extra_context or {}),
+        }
+
+        request.current_app = self.admin_site.name
+
+        return TemplateResponse(request, self.change_list_template or [
+            'admin/%s/%s/change_list.html' % (app_label, opts.model_name),
+            'admin/%s/change_list.html' % app_label,
+            'admin/change_list.html'
+        ], context)
 
     @staticmethod
     def link_name_fa(obj, table='language'):
@@ -193,12 +442,12 @@ class BrandAdmin(Base):
 
 
 class CategoryAdmin(Base):
-    list_display = ('parent', 'box', 'name_fa', 'deleted_by') + SafeDeleteAdmin.list_display
-    list_filter = ('name',) + SafeDeleteAdmin.list_filter
-    list_display_links = ('box',)
-    search_fields = ['name']
+    list_display = ('id', 'parent_id', 'box_id', 'name_fa') + SafeDeleteAdmin.list_display
+    list_filter = SafeDeleteAdmin.list_filter
+    list_display_links = ('name_fa',)
+    search_fields = ['name', 'id']
     list_per_page = 10
-    ordering = ('-created_at',)
+    # ordering = ('-created_at',)
 
 
 class CharityAdmin(Base):
@@ -368,7 +617,7 @@ class ProductAdmin(Base):
         return queryset, use_distinct
 
     def product_name(self, obj):
-        return obj.name['fa']
+        return obj.name.get("fa", "")
 
     product_name.short_description = 'name'
 
@@ -490,6 +739,64 @@ class TagAdmin(Base):
     list_display = ('id', 'name_fa', 'permalink') + SafeDeleteAdmin.list_display
     list_per_page = 10
     search_fields = ['name']
+
+    def get_queryset(self, request):
+        import inspect
+        print(inspect.stack()[1])
+
+        try:
+            queryset = self.model.all_objects.all()
+        except:
+            queryset = self.model._default_manager.all()
+
+        ordering = self.get_ordering(request)
+        if ordering:
+            queryset = queryset.order_by(*ordering)
+        return queryset
+
+    def get_object(self, request, object_id, from_field=None):
+        print(request.path)
+        """
+        Return an instance matching the field and value provided, the primary
+        key is used if no field is provided. Return ``None`` if no match is
+        found or the object_id fails validation.
+        """
+        queryset = self.get_queryset(request)
+        print('1')
+        model = queryset.model
+        field = model._meta.pk if from_field is None else model._meta.get_field(from_field)
+        try:
+            object_id = field.to_python(object_id)
+            return queryset.get(**{field.name: object_id})
+        except (model.DoesNotExist, ValidationError, ValueError):
+            return None
+
+    def get_changelist_instance(self, request):
+        """
+        Return a `ChangeList` instance based on `request`. May raise
+        `IncorrectLookupParameters`.
+        """
+        list_display = self.get_list_display(request)
+        list_display_links = self.get_list_display_links(request, list_display)
+        # Add the action checkboxes if any actions are available.
+        if self.get_actions(request):
+            list_display = ['action_checkbox', *list_display]
+        sortable_by = self.get_sortable_by(request)
+        return MyChangeList(
+            request,
+            self.model,
+            list_display,
+            list_display_links,
+            self.get_list_filter(request),
+            self.date_hierarchy,
+            self.get_search_fields(request),
+            self.get_list_select_related(request),
+            self.list_per_page,
+            self.list_max_show_all,
+            self.list_editable,
+            self,
+            sortable_by,
+        )
 
 
 class InvoiceStorageAdmin(admin.ModelAdmin):
@@ -615,3 +922,17 @@ for item in register_list:
     admin.site.register(*item)
 admin.site.site_header = "Mehr Takhfif"
 admin.site.site_title = "Mehr Takhfif"
+
+from jet.templatetags.jet_tags import assignment_tag
+
+
+@assignment_tag(takes_context=True)
+def jet_previous_object(context):
+    # return jet_sibling_object(context, False)
+    return "prev_obj"
+
+
+@assignment_tag(takes_context=True)
+def jet_next_object(context):
+    # return jet_sibling_object(context, True)
+    return "next_obj"
