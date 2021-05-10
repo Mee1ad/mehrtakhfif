3a21666eb8032d3f8b54f66befc202fa3f488e67
Meelad <soheilravasani@gmail.com>
2021-05-10 12:24:25 +0430

    add celery backup job

--- mehr_takhfif/celery.py
+++ mehr_takhfif/celery.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import, unicode_literals
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mehr_takhfif.settings')
@@ -19,3 +20,14 @@ app.config_from_object('mehr_takhfif.celeryconfig')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+app.conf.beat_schedule = {
+    "backup": {
+        "task": "core.tasks.backup",
+        "schedule": crontab(hour=15, minute=00)
+    },
+    "mediabackup": {
+        "task": "core.tasks.mediabackup",
+        # "schedule": crontab(hour=15, minute=15, day_of_week=1)
+        "schedule": crontab(hour=15, minute=15)
+    },
+}
